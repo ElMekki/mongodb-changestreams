db7ed3b60740b900dbda51348494f574a61cdb02
El Mekki <elmefr@yahoo.fr>
2023-05-29 11:58:43 +0200

    parmas modif. emo

--- src/main/java/com/paoue74/changestreams/JobChangeStreamService.java
+++ src/main/java/com/paoue74/changestreams/JobChangeStreamService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.ChangeStreamOptions;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
@@ -23,9 +24,10 @@ public class JobChangeStreamService {
 
   public Flux<Notification> watchForJobCollectionChanges() {
     ChangeStreamOptions options = ChangeStreamOptions.builder()
-        .filter(newAggregation(Job.class, match(where("operationType").in("insert"))))
+        .filter(newAggregation(Job.class, match(where("operationType").in("insert", "update","replace"))
+        ))
         .build();
-
+//                Aggregation.project("id", "fullDocument", "ns", "documentKey")
     // return a flux that watches the changestream and returns a notification object
     return reactiveMongoTemplate.changeStream("jobs", options, Job.class)
         .filter(Objects::nonNull)
